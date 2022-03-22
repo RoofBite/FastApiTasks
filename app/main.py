7cda1999551537d4eeedfd71bbc826965a43d1da
Przemysław Maryńczak <przemyslaw.marynczak@netguru.com>
2022-03-22 10:44:43 +0100

    Possible fix for "Table does not exists"

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from app.database import Base, engine
 from app.endpoints import router
 
+Base.metadata.create_all(engine)
 app = FastAPI()
 
 app.include_router(router)
